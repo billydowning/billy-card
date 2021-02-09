4eda5d450eda9951234d8e4a95d576f2c32c7114
Billy Downing <wldowning@gmail.com>
2021-02-09 13:52:06 -0700

    SwiftUI module. Built and designed business card app

--- BillyCard/ContentView.swift
+++ BillyCard/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.18, green: 0.80, blue: 0.44)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("billy-1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Billy Downing")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "385-295-0484", imageName: "phone.fill")
+                InfoView(text: "billy@gmail.com", imageName: "envelope.fill")
+            }
+            
+        }
+        
+            
     }
 }
 
@@ -19,3 +44,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ BillyCard/InfoView.swift
@@ -0,0 +1,32 @@
+//
+//  InfoView.swift
+//  BillyCard
+//
+//  Created by William Downing on 2/9/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
